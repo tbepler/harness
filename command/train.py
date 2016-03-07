9bd12a49e6d07fa107ec961ce59c72adeba8fefc
tbepler <tbepler@gmail.com>
2016-03-07 12:04:59 -0500

    added best method for snapshoting models

--- command/train.py
+++ command/train.py
@@ -18,12 +18,11 @@ def init_parser(parser):
     parser.add_argument('--max-length', dest='max_len', type=int, default=None, help='maximum length at which input sequences should be truncated (default: No truncation)')
     parser.add_argument('--epochs', type=int, default=100, help='number of iterations through the training data (default: 100)')
     parser.add_argument('--bptt', dest='bptt', type=int, default=2000, help='the length at which BPTT should be truncated, 0 indicates full BPTT (default: 128)')
-    parser.add_argument('--snapshot-prefix', dest='save_prefix', default=None, help='path prefix where model snapshots should be saved (default: snapshots/model)')
-    parser.add_argument('--snapshot-every', dest='snapshot_every', type=int, default=10, help='snapshot frequency in epochs (default: 10)')
     parser.add_argument('--batch-size', dest='batch_size', type=int, default=32, help='number of sequenes per batch (default: 32)')
     parser.add_argument('--fragment', dest='fragment', type=int, default=0, help='length into which input sequences should be fragmented, 0 indicates no fragmentation (default: 0)')
     parser.add_argument('--float', dest='dtype', choices=['32','64','default'], default='default', help='number of bits to use for floating point values')
     parser.add_argument('--validate', dest='validate', default='on', choices=['on','off'], help='whether to partition data into training and validation sets when training (default: on)')
+    save_parser(parser)
 
 def run(args):
     #check whether specific float size was specified
@@ -35,10 +34,7 @@ def run(args):
         dtype = None
     inputs = len(genrnn.util.dna.nucleotides)
     model, model_name, epoch = genrnn.util.model.load_model(args.model, inputs, inputs-1, dtype=dtype)
-    if args.save_prefix is None:
-        save_prefix = os.path.join('snapshots', model_name)
-    else:
-        save_prefix = args.save_prefix
+    saver = load_saver(args, model_name)
     #load data
     data = genrnn.util.dna.load_data(args.files, args.fragment)
     print "Training", args.model
@@ -62,9 +58,8 @@ def as_matrix(data):
         mat[:len(s),j:j+1] = s
     return mat
             
-def train(model, data, epochs, bptt, batch_size, save_prefix, save_freq
+def train(model, data, epochs, bptt, batch_size, saver
           , start_epoch=0, do_validate=True):
-    save_file_format = '{}_epoch{:06}_loss{:.3f}_acc{:.3f}.bin'
     start_epoch += 1
     epochs += start_epoch-1
     if do_validate:
@@ -115,11 +110,45 @@ def train(model, data, epochs, bptt, batch_size, save_prefix, save_freq
             sys.stdout.flush()
             model.reset()
         #check epoch and save model
-        if epoch % save_freq == 0:
-            save_path = save_file_format.format(save_prefix, epoch, err, acc)
-            with open(save_path, 'w') as f:
-                pickle.dump(model, f)
-    save_path = '{}_final_loss{:.3f}_acc{:.3f}.bin'.format(save_prefix, err, acc)
-    with open(save_path, 'w') as f:
-        pickle.dump(model, f)
+        saver.save(model, epoch, err, acc, final=(epoch==epochs))
 
+def save_parser(parser):
+    parser.add_argument('--snapshot-method', dest='save_method', choices=['best','every'], default='best', help='method to use for saving models (default=best)')
+    parser.add_argument('--snapshot-prefix', dest='save_prefix', default=None, help='path prefix where model snapshots should be saved (default: snapshots/model)')
+    parser.add_argument('--snapshot-every', dest='snapshot_every', type=int, default=10, help='snapshot frequency in epochs, if snapshot-method=every (default: 10)')
+
+def load_saver(args, model_name):
+    if args.save_prefix is None:
+        prefix = os.path.join('snapshots', model_name)
+    else:
+        prefix = args.save_prefix
+    save_format = ''.join([prefix, '_epoch{:06}_loss{:.3f}_acc{:.3f}.bin'])
+    if args.save_method == 'best':
+        return BestSaver(save_format)
+    elif args.save_method == 'every':
+        return EverySaver(save_format, args.snapshot_every)
+
+class BestSaver(object):
+    def __init__(self, save_format):
+        self.save_format = save_format
+        self.error = float('inf')
+
+    def save(self, model, epoch, err, acc, final=False):
+        if err < self.error:
+            path = self.save_format.format(epoch, err, acc)
+            with open(path, 'w') as f:
+                pickle.dump(model, f)
+            self.error = err
+
+class EverySaver(object):
+    def __init__(self, save_format, every):
+        self.save_format = save_format
+        self.every = every
+
+    def save(self, model, epoch, err, acc, final=False):
+        if final or epoch % self.every == 0:
+            path = self.save_format.format(epoch, err, acc)
+            with open(path, 'w') as f:
+                pickle.dump(model, f)
+
+        
